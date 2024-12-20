3563c90b835e7dc208acab4ef83d12f52b9fa341
Boubacar12486 <ba9218788@gmail.com>
2024-12-20 10:52:21 +0100

    update

--- CALCULATRICE.C
+++ CALCULATRICE.C
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <string.h>
-//fonction somme
+//fonction somme pour khadro
 int somme (int a, int b){
     return a+b
 }
@@ -12,4 +12,14 @@ float multiplier(float a,float b){
 //fonction souostraction pour Adja Penda
 float soustraction(float a,float b){
     return a-b
+}
+// methode main pour khadro,boobs,penda
+int main(int a,int b)
+{
+    a=2;
+    b=3;
+ int somme (a,b) ;
+ float multiplier (a,b) ;
+ float soustraction (a,b) ;
+
 }
