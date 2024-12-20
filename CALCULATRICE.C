84f78d57cacb7ea85f33cd4491bce360cb438fee
Adjaf42 <adjaf422@gmail.com>
2024-12-20 09:45:08 +0000

    Update CALCULATRICE.C

--- CALCULATRICE.C
+++ CALCULATRICE.C
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 //fonction somme
+int somme (int a, int b){
+    return a+b
+}
 
 //fonction produit pour boubacar
 float multiplier(float a,float b){
     return a*b
 }
-//fonction souostraction
+//fonction souostraction pour Adja Penda
+float soustraction(float a,float b){
+    return a-b
+}
